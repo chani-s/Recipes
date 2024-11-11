e502f41dc02bdc7ca6af8f940add3777ba95b212
chani-s <slaterchana@gmail.com>
2024-11-11 12:15:29 +0200

    add recipes cards

--- src/app/api/recipes/route.ts
+++ src/app/api/recipes/route.ts
@@ -1,14 +1,3 @@
-// // route.ts
-// import { NextResponse } from "next/server";
-// import { getAllDocuments, connectDatabase, insertDocument } from "../../services/mongo";
-
-// export async function GET(request : Request){
-//     const client = await connectDatabase();
-//     const recipes = await getAllDocuments(client, 'Recipe');
-//     await client.close();
-//     console.log(recipes);
-//     return NextResponse.json(recipes);
-// }
 
 
 import { NextResponse } from "next/server";
