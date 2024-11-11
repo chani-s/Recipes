a71651a25e5c0b2e9df734e06c53f6cc2c3711c4
AyalaSprei <ayala0bsd@gmail.com>
2024-11-11 12:17:25 +0200

    my diffrence

--- src/app/api/recipes/route.ts
+++ src/app/api/recipes/route.ts
@@ -1,5 +1,19 @@
+<<<<<<< HEAD
 
 
+=======
+// // route.ts
+// import { NextResponse } from "next/server";
+// import { getAllDocuments, connectDatabase, insertDocument } from "../../services/mongo";
+
+// export async function GET(request : Request){
+//     const client = await connectDatabase();
+//     const recipes = await getAllDocuments(client, 'Recipe');
+//     await client.close();
+//     console.log(recipes);
+//     return NextResponse.json(recipes);
+// }
+>>>>>>> ec4a8af (my diffrence)
 import { NextResponse } from "next/server";
 import { connectDatabase, getAllDocuments } from "../../services/mongo";
 
@@ -7,8 +21,8 @@ export async function GET() {
   try {
     const client = await connectDatabase();
     const recipes = await getAllDocuments(client, 'Recipe');
-    console.log(recipes)
     await client.close();
+    console.log(recipes)
     return NextResponse.json(recipes);
   } catch (error) {
     console.error("Error fetching recipes:", error);
