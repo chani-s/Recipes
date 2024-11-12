f7974b4741b5c5320d255d5bafe55f7a17548c62
chani-s <slaterchana@gmail.com>
2024-11-12 12:21:58 +0200

    set components

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,111 +1,50 @@
-//recipes/page.tsx
 "use client"
 import recipeService from "../services/recipes";
 import { Recipe } from "../../models/recipe";
 import { useEffect, useState } from "react";
 import styles from './recipes.module.css';
-import PageSidebar from "../PageSidebar/PageSidebar";
-
+import RecipeCard from "../components/RecipeCard/RecipeCard";
 
 const Page = () => {
+    const [recipes, setRecipes] = useState<Recipe[]>([]);
+    const [loading, setLoading] = useState(true);
+    const getRecipes = async () => {
+        try {
+            const recipesData = await recipeService.getAllRecipes();
+            setRecipes(recipesData);
+            console.log("Fetched recipes:", recipesData);
+        } catch (error: any) {
+            console.log("Error fetching recipes:", error.message);
+        }
+        finally {
+            setLoading(false);
+        }
+    };
+    useEffect(() => {
+        getRecipes();
+    }, []);
 
-  const [recipes, setRecipes] = useState<Recipe[]>([]);
-  const [selectedRecipe, setSelectedRecipe] = useState<Recipe | undefined>(undefined);
-  const [loading, setLoading] = useState(true); // New loading state
 
-
-
-  const getRecipes = async () => {
-    try {
-      const recipesData = await recipeService.getAllRecipes();
-      setRecipes(recipesData);
-      console.log("Fetched recipes:", recipesData);
-    } catch (error: any) {
-      console.log("Error fetching recipes:", error.message);
-    }
-    finally {
-      setLoading(false); // Set loading to false once data is fetched or an error occurs
-    }
-  };
-
-<<<<<<< HEAD
     return (
-        <div className={styles.pageContainer}>
-          {loading ? ( // Check if data is still loading
-            <p className={styles.loader}>LOADING...</p>
-          ) : (
-            <div className={styles.recipesGrid}>
-              {recipes.map((recipe) => (
-                <div
-                  key={recipe.id instanceof Object ? recipe.id.toString() : 'default-key'}
-                  className={styles.recipeContainer}
-                  onClick={() => openSidebar(recipe)}
-                >
-                  <h2 className={styles.recipeTitle}>{recipe.title}</h2>
-                  {recipe.image && (
-                    <img
-                      src={recipe.image}
-                      alt={recipe.title}
-                      className={styles.recipeImage}
-                    />
-                  )}
-                </div>
-              ))}
-=======
-  useEffect(() => {
-    getRecipes();
-  }, []);
+        <div>
+            <div className={styles.pageContainer}>
+                {loading ? (
+                    <p>LOADING...</p>
+                ) : (
+                    <div className={styles.recipesGrid}>
+                        {recipes.map((recipe, index) => (
+                            <RecipeCard recipe={recipe} index={index} />
+                        ))}
+                    </div>
 
-  const openSidebar = (recipe: Recipe) => {
-    console.log("in click");
-    setSelectedRecipe(recipe);
-  };
+                )}
 
-  const closeSidebar = () => {
-    setSelectedRecipe(undefined);
-  };
-
-
-  return (
-    <div className={styles.pageContainer}>
-      {loading ? ( // Check if data is still loading
-        <p>LOADING...</p>
-      ) : (
-        <div className={styles.recipesGrid}>
-          {recipes.map((recipe, index) => (
-            <div
-              key={index}
-              className={styles.recipeContainer}
-              onClick={() => openSidebar(recipe)}
-            >
-              <h2 className={styles.recipeTitle}>{recipe.title}</h2>
-              {recipe.image && (
-                <img
-                  src={recipe.image}
-                  alt={recipe.title}
-                  className={styles.recipeImage}
-                />
-              )}
->>>>>>> origin/main
             </div>
-
-          ))}
         </div>
-      )}
 
-      {selectedRecipe && (
-        <div className={styles.sidebar}>
-          <PageSidebar recipe={selectedRecipe} onClose={closeSidebar} />
-
-        </div>
-      )}
-    </div>
-  );
+    );
 };
-
-
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/main
 export default Page;
+
+
+
